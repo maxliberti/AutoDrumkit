7d7aa951f0e38490a62391ce54bceb9b70a3b75b
maxliberti <maxdliberti@gmail.com>
2024-12-29 20:28:55 -0500

    Organized main into classes

--- main.py
+++ main.py
@@ -4,63 +4,74 @@ import zipfile
 import ctypes, sys
 import logging
 
-# logging config
-logging.basicConfig(filename='organize_kits.log', filemode='w', level=logging.DEBUG, format='%(asctime)s ; %(levelname)s ; %(message)s')
+class Utils():
+    def __init__(self):
+        # logging config
+        logging.basicConfig(filename='organize_kits.log', filemode='w', level=logging.DEBUG, format='%(asctime)s ; %(levelname)s ; %(message)s')
 
-# paths
-download_path = os.path.join(os.path.expanduser('~'), 'Downloads')
+        # paths
+        self.download_path = os.path.join(os.path.expanduser('~'), 'Downloads')
 
-# checks if user is admin
-def is_user_admin():
-    try:
-        return ctypes.windll.shell32.IsUserAnAdmin()
-    except:
-        return False
+    # checks if user is admin
+    def is_user_admin(self):
+        try:
+            return ctypes.windll.shell32.IsUserAnAdmin()
+        except:
+            return False
 
-def print_files_in_folder(ext, path): # debug function
-  for root, dirs, files in os.walk(path):
-    for name in files:
-      if name.endswith(ext):
-        print(name)  # printing file name
+    def print_files_in_folder(ext, path):  # debug function
+        for root, dirs, files in os.walk(path):
+            for name in files:
+                if name.endswith(ext):
+                    print(name)  # printing file name
 
-def organize_kits(path):
-    logging.debug("Started organizing kits")
-    for filename in os.listdir(path):
-        src = path + "/" + filename # Original zip file path
+class AutoDrumkit():
+    def __init__(self):
+        pass
 
-        if "kit" in filename.lower() and filename.endswith(".zip"):
-            # paths
-            folder_name = os.path.splitext(filename)[0] # folder name without .zip at the end
-            folder_for_kits = "C:\\Program Files\\Image-Line\\FL Studio 2024\\Data\\Patches\\Packs"
-            new_folder_path = os.path.join(folder_for_kits, folder_name) # path that will contain the kit
-            zip_destination = os.path.join(new_folder_path, filename) # path where the zip file is moved
+    def organize_kits(self, path):
+        logging.debug("Started organizing kits")
+        for filename in os.listdir(path):
+            src = path + "/" + filename # Original zip file path
 
-            # make directory
-            os.makedirs(new_folder_path, exist_ok=True)
-            logging.debug(f"Created directory: {new_folder_path}")
+            if "kit" in filename.lower() and filename.endswith(".zip"):
+                try:
+                    # paths
+                    folder_name = os.path.splitext(filename)[0] # folder name without .zip at the end
+                    folder_for_kits = "C:\\Program Files\\Image-Line\\FL Studio 2024\\Data\\Patches\\Packs"
+                    new_folder_path = os.path.join(folder_for_kits, folder_name) # path that will contain the kit
+                    zip_destination = os.path.join(new_folder_path, filename) # path where the zip file is moved
 
-            # move the file
-            shutil.move(src, new_folder_path)
-            logging.debug(f"Moved zip to: {new_folder_path}")
+                    # make directory
+                    os.makedirs(new_folder_path, exist_ok=True)
+                    logging.debug(f"Created directory: {new_folder_path}")
 
-            # unzip
-            with zipfile.ZipFile(zip_destination, "r") as zip_ref:
-                zip_ref.extractall(new_folder_path)
+                    # move the file
+                    shutil.move(src, new_folder_path)
+                    logging.debug(f"Moved zip to: {new_folder_path}")
 
-            # delete original zip file
-            os.remove(zip_destination)
-            logging.debug(f"Extracted and deleted zip: {zip_destination}")
+                    # unzip
+                    with zipfile.ZipFile(zip_destination, "r") as zip_ref:
+                        zip_ref.extractall(new_folder_path)
 
-    logging.debug("Finished organizing kits")
+                    # delete original zip file
+                    os.remove(zip_destination)
+                    logging.debug(f"Extracted and deleted zip: {zip_destination}")
+                except Exception as e:
+                    logging.error(f"Error organizing kit {filename}: {e}")
 
-def main():
-    if not is_user_admin():
-        # Re-run the program with admin rights
-        ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, __file__, None, 1)
-        return
-    logging.debug("Starting the script with admin rights")
-    organize_kits(download_path)
+        logging.debug("Finished organizing kits")
+
+    def main(self):
+        utils = Utils()
+        if not utils.is_user_admin():
+            # Re-run the program with admin rights
+            ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, __file__, None, 1)
+            return
+        logging.debug("Starting the script with admin rights")
+        self.organize_kits(utils.download_path)
 
 
 if __name__ == '__main__':
-  main()
+    auto_drumkit = AutoDrumkit()
+    auto_drumkit.main()
